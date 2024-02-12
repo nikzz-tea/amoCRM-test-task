1f78ffb388cc328d0267d6b564893f2aa2dc75b6
nikzz-tea <nikita.red638@gmail.com>
2024-02-13 02:11:32 +0300

    [frontend] feat: add custom select

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,1 +1,16 @@
-<template>Main</template>
+<script setup lang="ts">
+import Select from "./components/Select.vue";
+
+const options = [
+  { name: "Сделка", value: "leads" },
+  { name: "Контакт", value: "contacts" },
+  { name: "Компания", value: "companies" },
+];
+</script>
+<template>
+  <div
+    class="mx-auto mt-16 flex min-h-[854px] w-[1200px] flex-col items-center gap-10 rounded-t-3xl bg-white p-8 shadow-lg"
+  >
+    <Select :options="options" />
+  </div>
+</template>
---
+++ client/src/components/Select.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+interface Entity {
+  name: string;
+  value: string;
+}
+
+defineProps<{
+  options: Entity[];
+}>();
+</script>
+
+<template>
+  <select
+    class="w-[300px] rounded-sm border border-neutral-400 bg-neutral-300 py-2 pl-3 pr-10 duration-200 sm:text-sm"
+  >
+    <option value="0">Не выбрано</option>
+    <option v-for="{ value, name } in options" :value="value" :key="value">
+      {{ name }}
+    </option>
+  </select>
+</template>
