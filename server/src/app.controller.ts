194a0fbee68712a738029c6047c02d0b3bbfa79a
nikzz-tea <nikita.red638@gmail.com>
2024-02-14 13:48:34 +0300

    [backend] feat: create method for adding entities

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,26 +1,22 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
-import { TokenService } from './token.service';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private readonly tokenService: TokenService,
-  ) {}
+  constructor(private readonly appService: AppService) {}
 
   @Post('/leads')
   createLead(@Body() dto: {}) {
-    return `"Created" entity ${JSON.stringify(dto)}`;
+    return this.appService.createEntity('leads', dto);
   }
 
   @Post('/contacts')
   createContact(@Body() dto: {}) {
-    return `"Created" entity ${JSON.stringify(dto)}`;
+    return this.appService.createEntity('contacts', dto);
   }
 
   @Post('/companies')
   createCompany(@Body() dto: {}) {
-    return `"Created" entity ${JSON.stringify(dto)}`;
+    return this.appService.createEntity('companies', dto);
   }
 }
--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
+import { TokenService } from './token.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly tokenService: TokenService) {}
+
+  async createEntity(entity: string, dto: {}) {
+    const { access_token, base_domain } =
+      await this.tokenService.getCredentials();
+    try {
+      const { data } = await axios.post(
+        `http://${base_domain}/api/v4/${entity}`,
+        dto,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${access_token}`,
+          },
+        },
+      );
+      return data;
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 }
